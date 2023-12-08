7cde73683e4783e1ace097c2e0287e929e012cd0
Jan Nash <jan@tape.it>
2023-12-08 22:26:38 +0100

    Refactor generate_favicon and add `:namespace` option

--- lib/generators/favicon_generator.rb
+++ lib/generators/favicon_generator.rb
@@ -6,35 +6,75 @@ require 'zip'
 require 'base64'
 
 class FaviconGenerator < Rails::Generators::Base
-  API_KEY = '04641dc33598f5463c2f1ff49dd9e4a617559f4b'
-
   PATH_UNIQUE_KEY = '/Dfv87ZbNh2'
 
   class_option(:timeout, type: :numeric, aliases: '-t', default: 30)
+  class_option(:namespace, type: :string, aliases: '-n', default: nil)
 
   def generate_favicon
-    if File.exist?('config/favicon.yml') && defined?(Rails.application.config_for)
-      req = Rails.application.config_for(:favicon)
-    else
-      req = JSON.parse File.read('config/favicon.json')
+    timeout = options[:timeout]
+    namespace = options[:namespace]
+
+    payload = prepare_payload(namespace)
+    response = send_post_request(payload, timeout)
+
+    zip = response['favicon_generation_result']['favicon']['package_url']
+    favicon_folder_path = ['app/assets/images', namespace, 'favicon'].distinct.join('/')
+    FileUtils.mkdir_p(favicon_folder_path)
+
+    Dir.mktmpdir 'rfg' do |tmp_dir|
+      download_package zip, tmp_dir
+      Dir["#{tmp_dir}/*.*"].each do |file|
+        content = File.binread(file)
+        new_ext = ''
+        if ['.json', '.xml', '.webmanifest'].include? File.extname(file)
+          content = replace_url_by_asset_path content
+          new_ext = '.erb'
+        end
+        create_file "#{favicon_folder_path}#{File.basename file}#{new_ext}", content
+      end
     end
 
-    req['api_key'] = API_KEY
+    # FIXME: FIXME:
+    create_file "app/views/application/_favicon.html.erb",
+      replace_url_by_asset_path(response['favicon_generation_result']['favicon']['html_code'])
 
-    req['files_location'] = Hash.new
-    req['files_location']['type'] = 'path'
-    req['files_location']['path'] = PATH_UNIQUE_KEY
+    create_file "config/initializers/web_app_manifest.rb",
+      File.read(File.dirname(__FILE__) + '/web_app_manifest_initializer.txt')
+  end
 
-    master_pic = File.expand_path(".") + '/' + req['master_picture']
-    req['master_picture'] = Hash.new
-    req['master_picture']['type'] = 'inline'
-    req['master_picture']['content'] = Base64.encode64(File.binread(master_pic))
+  private
 
+  def prepare_payload(namespace)
+    favicon_config_folder = "#{File.expand_path('.')}/config/rails_real_favicon"
+    if namespace
+      favicon_config_folder += "/#{namespace}"
+    end
+    
+    favicon_json_path = "#{favicon_config_folder}/favicon.json"
+    payload = JSON.parse File.read(favicon_json_path)
+
+    master_pic_path = "#{favicon_config_folder}/#{payload['master_picture']}"
+    master_pic_enc = Base64.encode64(File.binread(master_pic_path))
+
+    payload['api_key'] = '04641dc33598f5463c2f1ff49dd9e4a617559f4b'
+
+    payload['files_location'] = Hash.new
+    payload['files_location']['type'] = 'path'
+    payload['files_location']['path'] = PATH_UNIQUE_KEY
+
+    payload['master_picture'] = Hash.new
+    payload['master_picture']['type'] = 'inline'
+    payload['master_picture']['content'] = master_pic_enc
+
+    return payload
+  end
+
+  def send_post_request(payload, timeout)
     uri = URI.parse("https://realfavicongenerator.net/api/favicon")
-    timeout = options[:timeout]
-    resp = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https", read_timeout: timeout) do |http|
+    return Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https", read_timeout: timeout) do |http|
       request = Net::HTTP::Post.new uri
-      request.body = { favicon_generation: req }.to_json
+      request.body = { favicon_generation: payload }.to_json
       request["Content-Type"] = "application/json"
       begin
         JSON.parse(http.request(request).body)
@@ -42,31 +82,7 @@ class FaviconGenerator < Rails::Generators::Base
         raise RuntimeError.new("Operation timed out after #{timeout} seconds, pass a `-t` option for a longer timeout")
       end
     end
-
-    zip = resp['favicon_generation_result']['favicon']['package_url']
-    FileUtils.mkdir_p('app/assets/images/favicon')
-
-    Dir.mktmpdir 'rfg' do |tmp_dir|
-      download_package zip, tmp_dir
-      Dir["#{tmp_dir}/*.*"].each do |file|
-        content = File.binread(file)
-        new_ext = ''
-        if File.extname(file) == '.json' or File.extname(file) == '.xml' or File.extname(file) == '.webmanifest'
-          content = replace_url_by_asset_path content
-          new_ext = '.erb'
-        end
-        create_file "app/assets/images/favicon/#{File.basename file}#{new_ext}", content
-      end
-    end
-
-    create_file "app/views/application/_favicon.html.erb",
-      replace_url_by_asset_path(resp['favicon_generation_result']['favicon']['html_code'])
-
-      create_file "config/initializers/web_app_manifest.rb",
-        File.read(File.dirname(__FILE__) + '/web_app_manifest_initializer.txt')
   end
-
-  private
 
   def download_package(package_url, output_dir)
     file = Tempfile.new('fav_package')
